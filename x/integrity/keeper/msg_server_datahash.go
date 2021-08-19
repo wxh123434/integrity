f245ca78f1bcbdac855969c18c0fa1a057711f77
wxh123434 <78358488+wxh123434@users.noreply.github.com>
2021-08-19 15:46:01 +0800

    Update msg_server_datahash.go

--- x/integrity/keeper/msg_server_datahash.go
+++ x/integrity/keeper/msg_server_datahash.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/SBC/integrity/x/integrity/types"
+	"github.com/wxh123434/integrity/x/integrity/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
