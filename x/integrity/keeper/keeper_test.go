2458ffb263d3a4f0855f6a48467e1cc99c331427
wxh123434 <78358488+wxh123434@users.noreply.github.com>
2021-08-19 15:45:22 +0800

    Update keeper_test.go

--- x/integrity/keeper/keeper_test.go
+++ x/integrity/keeper/keeper_test.go
@@ -3,7 +3,7 @@ package keeper
 import (
 	"testing"
 
-	"github.com/SBC/integrity/x/integrity/types"
+	"github.com/wxh123434/integrity/x/integrity/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/cosmos/cosmos-sdk/store"
